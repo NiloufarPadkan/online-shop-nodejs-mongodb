4041472f18bb84be7d102953a918aa94b17c5a6d
NiloufarPadkan <niloufar.pa@gmail.com>
2021-10-16 16:10:48 +0330

    manage and confirm products added

--- routers/order.js
+++ routers/order.js
@@ -23,4 +23,38 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    if (req.params.id) {
+      const order = await Order.findById(req.params.id);
+      return res.status(200).send(order);
+    }
+    const orders = await Order.find();
+    return res.status(200).send(orders);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/pending", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const order = await Order.find({ status: "pending" });
+    return res.status(200).send(order);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+router.post("/confirm/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const confirmedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      { status: "confirmed" },
+
+      { new: true }
+    );
+    return res.status(200).send(confirmedOrder);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 module.exports = router;
